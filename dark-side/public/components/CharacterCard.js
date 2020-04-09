32d98a548622e140dfc6c05bf2d3e76bfaf63085
Chelsea Wetzel <chelswetz@gmail.com>
2020-04-09 02:45:44 -0400

    star wars API down

---
+++ dark-side/public/components/CharacterCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+// you can build out an empty component as long as it has a return with an empty div
+// this will help to get your App layout setup and really make you think like a developer
+
+// make sure that the name of this component ALWAYS matches your file name! 
+// you can write your function like this...or like this: 
+// function CharacterCard() {
+
+// }
+// if you write it with the function keyword, you can then export your component in the same line rather than at the bottom
+const CharacterCard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CharacterCard;
--- dark-side/src/App.js
+++ dark-side/src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
+import axios from "axios";
+// step 1: build out components and think about layout
+// step 2: install axios in your dark-side file and import it
+// step 3: change app to a class component - use constructor and super
+// set initial state to an empty array assigned the name characters
+// put a render surrounding your return statement
+// step 4: create a componentDidMount and put axios call inside there
 
-function App() {
-  return (
-    <div className="App">
-      <h1>"The Force will be with you. Always" - Obi-Wan Kenobi</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      characters: []
+    }
+  };
+
+  componentDidMount() {
+    axios
+    .get(`https://rickandmortyapi.com/api/character/`)
+    .then(res => {
+      this.setState({
+        characters: res.data
+      })
+      console.log(res.data)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>"The Force will be with you. Always" - Obi-Wan Kenobi</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
